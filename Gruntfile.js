f87b4f599b7a90adfa2e228881aa0496f395001d
Manuel Rodriguez <rodriguez.manu@gmail.com>
2016-11-08 11:24:51 -0300

    changing name module

--- Gruntfile.js
+++ Gruntfile.js
@@ -283,7 +283,7 @@ module.exports = function(grunt) {
         ngtemplates : {
           options : {
             // This should be the name of your apps angular module
-            module : 'DemoApp',
+            module : 'Blog',
             htmlmin : {
               collapseBooleanAttributes : true,
               collapseWhitespace : true,
